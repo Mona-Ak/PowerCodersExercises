cee995b0526fbc0cbd24f5e0619039ebd30a8649
Mona <monaakbarpour1@gmail.com>
2022-04-19 15:47:15 +0200

    finishing Say Hello exercise

---
+++ introductionToJS/sayHello/main.js
@@ -0,0 +1,9 @@
+let display = document.querySelector('p');
+let names = prompt('add your name please', 'mona');
+
+if(names != null){
+    display.innerHTML = `Hello ${names}. welcome to my world.`
+}
+
+
+
