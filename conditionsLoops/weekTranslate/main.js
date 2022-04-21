f90275aa444bc46073f44fcf44a92aff5e20d5dc
Mona <monaakbarpour1@gmail.com>
2022-04-21 13:49:55 +0200

    I tried to use switch but it didn't work

--- conditionsLoops/weekTranslate/main.js
+++ conditionsLoops/weekTranslate/main.js
@@ -4,44 +4,86 @@ let daysList = document.querySelectorAll('li');
 let days = [];
 let btn = document.querySelector('button');
 
-let events = btn.addEventListener('click',()=>{
-    for (let i = 0; i < daysList.length; i++) {    
-        days.push(daysList[i]); 
-       console.log(days[i].textContent)
+// let events = btn.addEventListener('click',()=>{
+//     for (let i = 0; i < daysList.length; i++) {    
+//         days.push(daysList[i]); 
+//        console.log(days[i].textContent)
     
-       if(days[i].textContent.includes('Monday')){
+//        if(days[i].textContent.includes('Monday')){
     
-           days[i].textContent += 'Montag';
+//            days[i].textContent += 'Montag';
     
-       }else if(days[i].textContent.includes('Tuesday')){
+//        }else if(days[i].textContent.includes('Tuesday')){
     
-           days[i].textContent += 'Dienstag';
+//            days[i].textContent += 'Dienstag';
     
-       }else if(days[i].textContent.includes('Wednesday')){
+//        }else if(days[i].textContent.includes('Wednesday')){
     
-           days[i].textContent += 'Mitwoch';
+//            days[i].textContent += 'Mitwoch';
     
-       }else if(days[i].textContent.includes('Thursday')){
+//        }else if(days[i].textContent.includes('Thursday')){
     
-           days[i].textContent += 'Donnerstag';
+//            days[i].textContent += 'Donnerstag';
     
-       }else if(days[i].textContent.includes('Friday')){
+//        }else if(days[i].textContent.includes('Friday')){
     
-           days[i].textContent += 'Freitag';
+//            days[i].textContent += 'Freitag';
     
-       }else if(days[i].textContent.includes('Saturday')){
+//        }else if(days[i].textContent.includes('Saturday')){
     
-           days[i].textContent += 'Samstag';
+//            days[i].textContent += 'Samstag';
     
-       }else if(days[i].textContent.includes('Sunday')){
+//        }else if(days[i].textContent.includes('Sunday')){
     
-           days[i].textContent += 'Sontag';
-       }
-       
+//            days[i].textContent += 'Sontag';
+//        }
+            
+//     } 
+
+// });
+
+// *************SWITCH***********************
+
+for (let i = 0; i < daysList.length; i++) {
+    days.push(daysList[i].textContent); 
+    console.log()
+}
+for (let i = 0; i < days.length; i++) {
+    console.log(days[i])
+    
+    switch(days[i]){
+        case 'Monday':
+        console.log('this is mon');
+        break;
+        case 'Tuesday':
+        console.log('this is tue')
+        break;
+        case 'Thursday':
+        console.log('this is thu')
+        break;
+        case 'Wednesday':
+        console.log('this is mon')
+        break;
+        case 'Friday':
+        console.log('this is mon')
+        break;
+        case 'Saturday':
+        console.log('this is mon')
+        break;
+        case 'Sonday':
+        console.log('this is mon')
+        break;
+        default :
+        // console.log('no sorry');
+        break;
         
     }
+   
     
+}
 
-})
+
+
+
 
 
