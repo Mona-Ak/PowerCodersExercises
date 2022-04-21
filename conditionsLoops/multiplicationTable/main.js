daddff29449cc6efe34bf6ea5b1ef4cf1ffcbf53
Mona <monaakbarpour1@gmail.com>
2022-04-21 16:00:57 +0200

    The part two is done

--- conditionsLoops/multiplicationTable/main.js
+++ conditionsLoops/multiplicationTable/main.js
@@ -1,9 +1,15 @@
-let proInp = prompt('please add a number between 0 - 10');
-let num = parseInt(proInp);
+// let proInp = prompt('please add a number between 0 - 10');
+// let num = parseInt(proInp);
 
-if( 0 < num && num < 10){
-    console.log( num * 9);
-}else{
-    console.log('please add a number between 0 - 10');
-}
+// if( 0 < num && num < 10){
+//     console.log(`${i} * 9 =` +' '+ num * 9);
+// }else{
+//     console.log('please add a number between 0 - 10');
+// }
+
+// *************multiplication table part 2***************
+
+for(let i = 0; i <= 10 ; i++){
+    console.log(`${i} * 9 =` +' '+ i * 9);
+};
 
