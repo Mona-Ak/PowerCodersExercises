1b4c01b61fc94f1d53fd1aefe5cd40a8e9935b89
Mona <monaakbarpour1@gmail.com>
2022-04-22 11:42:45 +0200

    greetings to the people who adds her/his name

---
+++ javascriptFunctions/sayHello/script.js
@@ -0,0 +1,11 @@
+// ******Say hello
+// ******Create a program in JavaScript that prompts for your name and prints a greeting using your name.
+
+let myName = prompt('What is your name');
+let greeting = (name)=>{
+    console.log(`Hello ${name}. wellcome here.`)
+}
+greeting(myName);
+
+
+
