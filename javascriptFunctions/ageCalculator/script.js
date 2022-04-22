3d2ecd9c4e98f241fc172924a538f76a7c94ad10
Mona <monaakbarpour1@gmail.com>
2022-04-22 16:14:44 +0200

    calculating the year of age

--- javascriptFunctions/ageCalculator/script.js
+++ javascriptFunctions/ageCalculator/script.js
@@ -4,42 +4,52 @@ let date = new Date();
 let year = date.getFullYear();
 let month = date.getMonth();
 let day = date.getDate();
-let myDay = prompt('add you day of birth');
-let myMonth = prompt('add you month of birth');
+// let myDay = prompt('add you day of birth');
+// let myMonth = prompt('add you month of birth');
 let myYear = prompt('add your year of birth');
 let display = document.querySelector('p');
 
 
+// age calculator in one function
+// let ageCalculate = (y,m,d)=>{
+//     let currentYear = year - y;
+//     let currentMonth;
+//     let currentDay;
+//     if(month < m ){
+//         currentMonth = month - m  + 12 +1;
+//         currentYear - 1;
+//         console.log('this is month' + [currentMonth]);
+//     }else{
+//         currentMonth = month - m ;
+//     }
 
-let ageCalculate = (y,m,d)=>{
-    let currentYear = year - y;
-    let currentMonth;
-    let currentDay;
-    if(month < m ){
-        currentMonth = month - m  + 12 +1;
-        currentYear - 1;
-        console.log('this is month' + [currentMonth]);
-    }else{
-        currentMonth = month - m ;
-    }
+//     if(day < d && daysNum[month] == 28 ){
+//         currentDay = day - d + 28;
 
-    if(day < d && daysNum[month] == 28 ){
-        currentDay = day - d + 28;
-
-    }else if(day < d && daysNum[month] == 30){
-        currentDay = day - d + 30;
-    }else if(day < d && daysNum[month] == 31 ){
-        currentDay = day - d + 31;
-    }else{
-        currentDay = day - d;
-    }
+//     }else if(day < d && daysNum[month] == 30){
+//         currentDay = day - d + 30;
+//     }else if(day < d && daysNum[month] == 31 ){
+//         currentDay = day - d + 31;
+//     }else{
+//         currentDay = day - d;
+//     }
 
     
-    console.log(`you are ${currentYear} years , ${currentMonth} months and ${currentDay} days `);
+//     console.log(`you are ${currentYear} years , ${currentMonth} months and ${currentDay} days `);
  
+// }
+
+// ageCalculate(myYear,myMonth,myDay);
+
+// **********version 2*************
+
+let yearCalculator = (y)=>{
+    let currentYear = year - y;
+    return currentYear;
 }
 
-ageCalculate(myYear,myMonth,myDay);
+yearCalculator(myYear);
+
 
 
 
