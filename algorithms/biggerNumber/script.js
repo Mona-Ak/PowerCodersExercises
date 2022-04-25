30e36463f2dda96ec905b57541c535216b52f800
Mona <monaakbarpour1@gmail.com>
2022-04-25 14:52:40 +0200

    comparing two numbers 5 and 10

---
+++ algorithms/biggerNumber/script.js
@@ -0,0 +1,8 @@
+let x = 5;
+let y = 10;
+
+console.log(Math.max(x,y));
+
+console.log(`The greater number of ${x} and ${y} is ${Math.max(x , y)}`);
+console.log(`The smaller number of ${x} and ${y} is ${Math.min(x , y)}`);
+
