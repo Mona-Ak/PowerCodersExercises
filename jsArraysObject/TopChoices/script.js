d82f05ef97cc2a0710f5c6575228cc3cab1fbf97
Mona <monaakbarpour1@gmail.com>
2022-04-26 16:48:34 +0200

    using different loop to go through array

---
+++ jsArraysObject/TopChoices/script.js
@@ -0,0 +1,24 @@
+let color = ['red', 'black','violet', 'white' ];
+// color.forEach(function(item,index){
+//     console.log(` my #${index + 1} favoriete color is ${item}`)
+// });
+// color.forEach((item,index) => {
+//     console.log(` my #${index + 1} favoriete color is ${item}`)
+// });
+
+// for(let i = 0; i < color.length;i++){
+    //     console.log(` my #${i + 1} favoriete color is ${color[i]}`)
+    
+    // }
+// let i = 0
+// while (i < color.length) {
+// console.log(` my #${i + 1} favoriete color is ${color[i]}`);
+// i++;
+// }
+
+for(const i of color){
+    console.log(`my favoriete color is ${i}`);
+    
+}
+
+
